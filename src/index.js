134f7a7a60ecf0c8f7bc3d89f7718852a23207a7
overloot <overloot@ya.ru>
2023-12-08 18:30:05 +0300

    char controller

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("Hello World!");
+import "./lib/canvas.js";
+import { clearCanvas, drawChar } from "./lib/canvas";
+
+const player = {
+    char: "@",
+    color: "white",
+    position: {
+    x: 0,
+    y: 0,
+    },
+    };
+    
+drawChar(player);
+
+let userInput = null;
+
+document.addEventListener("keydown", (ev) => {
+  userInput = ev.key;
+  processUserInput();
+});
+
+const processUserInput = () => {
+  if (userInput === "ArrowUp") {
+    player.position.y -= 1;
+  }
+  if (userInput === "ArrowRight") {
+    player.position.x += 1;
+  }
+  if (userInput === "ArrowDown") {
+    player.position.y += 1;
+  }
+  if (userInput === "ArrowLeft") {
+    player.position.x -= 1;
+  }
+
+  clearCanvas();
+  drawChar(player);
+};
